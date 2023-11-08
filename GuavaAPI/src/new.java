d53eac9a7ee8455d56866cb01eba5a5533ec4def
Ashwani K Sharma <aksmagic@users.noreply.github.com>
2023-11-08 10:28:28 +0530

    new.java

--- GuavaAPI/src/new.java
+++ GuavaAPI/src/new.java
@@ -7,3 +7,12 @@
 			if (!file.exists())
 				file.mkdir();
 			sOutputFile = sFilePath+"\\"+sFileName;
+
+----
+	e  25: 	BufferedWriter bwWriter = null;
+	Line  41: 		bwWriter       = new BufferedWriter( new FileWriter(sDumpFileName, true));	 //Opening Dump file in APPEND mode
+	Line  63: 			bwWriter.write(sbDumpHearder.toString()+"\n");
+	Line  70: 					bwWriter.write(sRowData+"\t");
+	Line  72: 				bwWriter.write("\n");     //moving to New line
+	Line  85: 				if (bwWriter != null) {
+	Line  86: 					bwWriter.close();
